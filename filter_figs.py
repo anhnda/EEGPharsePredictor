8701b945114751c466726ab2e4c35f4bb7ff721a
NDA <anh.andyng@gmail.com>
2024-01-16 14:20:27 +0700

    Update

--- filter_figs.py
+++ filter_figs.py
@@ -5,10 +5,11 @@ import shutil
 def get_info(path):
     file_name = path.split("/")[-1]
     parts = file_name.split("_")
-    true_lb = parts[3]
-    pred_lb = parts[6].split(".")[0]
+    true_lb = parts[5]
+    pred_lb = parts[8].split(".")[0]
     fid = parts[0]
-    return fid, true_lb, pred_lb
+    eid = parts[2]
+    return fid, eid, true_lb, pred_lb
 
 def rep_star(s):
     return s.replace("*", "Star")
@@ -16,13 +17,13 @@ def filter_figs():
     ensureDir("Fig_StarWrong")
     ensureDir("Fig_NonStarWrong")
     for path in glob.glob("figs/*.png"):
-        fid, true_lb, pred_lb = get_info(path)
+        fid, eid, true_lb, pred_lb = get_info(path)
         if true_lb != pred_lb:
             print(true_lb, pred_lb)
             if true_lb.__contains__("*"):
-                shutil.copy(path, "Fig_StarWrong/"+fid+"_"+rep_star(true_lb)+"_"+rep_star(pred_lb)+".png")
+                shutil.copy(path, "Fig_StarWrong/"+eid+"_"+rep_star(true_lb)+"_"+rep_star(pred_lb)+".png")
             else:
-                shutil.copy(path, "Fig_NonStarWrong/"+fid+"_"+rep_star(true_lb)+"_"+rep_star(pred_lb)+".png")
+                shutil.copy(path, "Fig_NonStarWrong/"+eid+"_"+rep_star(true_lb)+"_"+rep_star(pred_lb)+".png")
 
 
 
