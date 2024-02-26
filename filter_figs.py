5bc8b5648a4c9b478a957dc392f720159821e65e
NDA <anh.andyng@gmail.com>
2024-02-26 17:09:51 +0700

    Update

--- filter_figs.py
+++ filter_figs.py
@@ -5,6 +5,8 @@ from utils import ensureDir
 import shutil
 from train import get_model_dirname, parse_x
 import params
+
+
 def get_info(path):
     file_name = path.split("/")[-1]
     parts = file_name.split("_")
@@ -14,13 +16,16 @@ def get_info(path):
     eid = parts[2]
     return fid, eid, true_lb, pred_lb
 
+
 def rep_star(s):
     return s.replace("*", "Star")
-def filter_figs():
-    fig_dir = get_model_dirname() + "/figs/" + params.TRAIN_ID + "_" + params.TEST_ID
 
-    fig_dir1 = get_model_dirname() + "/Fig_StarWrong/" + params.TRAIN_ID + "_" + params.TEST_ID
-    fig_dir2 = get_model_dirname() + "/Fig_NonStarWrong/" + params.TRAIN_ID + "_" + params.TEST_ID
+
+def filter_figs():
+    fig_dir = get_model_dirname() + "/figs/" + "%s" % params.TRAIN_ID + "_" + "%s" % params.TEST_ID
+
+    fig_dir1 = get_model_dirname() + "/Fig_StarWrong/" + "%s" % params.TRAIN_ID + "_" + "%s" % params.TEST_ID
+    fig_dir2 = get_model_dirname() + "/Fig_NonStarWrong/" + "%s" % params.TRAIN_ID + "_" + "%s" % params.TEST_ID
 
     os.system("rm -rf " + fig_dir1)
     os.system("rm -rf " + fig_dir2)
@@ -38,9 +43,9 @@ def filter_figs():
         if true_lb != pred_lb:
             print(true_lb, pred_lb)
             if true_lb.__contains__("*"):
-                shutil.copy(path, fig_dir1 + "/"+eid+"_"+rep_star(true_lb)+"_"+rep_star(pred_lb)+".png")
+                shutil.copy(path, fig_dir1 + "/" + eid + "_" + rep_star(true_lb) + "_" + rep_star(pred_lb) + ".png")
             else:
-                shutil.copy(path, fig_dir2 +  "/"+eid+"_"+rep_star(true_lb)+"_"+rep_star(pred_lb)+".png")
+                shutil.copy(path, fig_dir2 + "/" + eid + "_" + rep_star(true_lb) + "_" + rep_star(pred_lb) + ".png")
     print(ns)
 
 
