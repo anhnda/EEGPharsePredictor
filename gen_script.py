d6e39b5f81eb25792c2cecfd3f7f7a49ff3fd615
NDA <anh.andyng@gmail.com>
2024-02-26 17:30:43 +0700

    Update

--- gen_script.py
+++ gen_script.py
@@ -11,11 +11,11 @@ def gen_script():
                 fout.write("python train.py " + regg +" "+ remg +" "+ rmot+"\n")
                 for tid in ["1", "2", "3"]:
                     fout.write("python xshap.py "+ regg +" "+ remg +" "+ rmot+
-                               " --train=={}  --test=={}\n".format(1, tid))
+                               " --train={}  --test={}\n".format(1, tid))
                     fout.write("python visualization2.py "+ regg +" "+ remg +" "+ rmot+
-                               " --train=={}  --test=={}\n".format(1, tid))
+                               " --train={}  --test={}\n".format(1, tid))
                     fout.write("python filter_figs.py "+ regg +" "+ remg +" "+ rmot+
-                               " --train=={}  --test=={}\n".format(1, tid))
+                               " --train={}  --test={}\n".format(1, tid))
                 fout.write("#################\n")
 
 
--- runx.sh
+++ runx.sh
@@ -1,77 +1,77 @@
 python train.py   
-python xshap.py    --train==1  --test==1
-python visualization2.py    --train==1  --test==1
-python filter_figs.py    --train==1  --test==1
-python xshap.py    --train==1  --test==2
-python visualization2.py    --train==1  --test==2
-python filter_figs.py    --train==1  --test==2
-python xshap.py    --train==1  --test==3
-python visualization2.py    --train==1  --test==3
-python filter_figs.py    --train==1  --test==3
+python xshap.py    --train=1  --test=1
+python visualization2.py    --train=1  --test=1
+python filter_figs.py    --train=1  --test=1
+python xshap.py    --train=1  --test=2
+python visualization2.py    --train=1  --test=2
+python filter_figs.py    --train=1  --test=2
+python xshap.py    --train=1  --test=3
+python visualization2.py    --train=1  --test=3
+python filter_figs.py    --train=1  --test=3
 #################
 python train.py   --rmot
-python xshap.py   --rmot --train==1  --test==1
-python visualization2.py   --rmot --train==1  --test==1
-python filter_figs.py   --rmot --train==1  --test==1
-python xshap.py   --rmot --train==1  --test==2
-python visualization2.py   --rmot --train==1  --test==2
-python filter_figs.py   --rmot --train==1  --test==2
-python xshap.py   --rmot --train==1  --test==3
-python visualization2.py   --rmot --train==1  --test==3
-python filter_figs.py   --rmot --train==1  --test==3
+python xshap.py   --rmot --train=1  --test=1
+python visualization2.py   --rmot --train=1  --test=1
+python filter_figs.py   --rmot --train=1  --test=1
+python xshap.py   --rmot --train=1  --test=2
+python visualization2.py   --rmot --train=1  --test=2
+python filter_figs.py   --rmot --train=1  --test=2
+python xshap.py   --rmot --train=1  --test=3
+python visualization2.py   --rmot --train=1  --test=3
+python filter_figs.py   --rmot --train=1  --test=3
 #################
 python train.py  --remg 
-python xshap.py  --remg  --train==1  --test==1
-python visualization2.py  --remg  --train==1  --test==1
-python filter_figs.py  --remg  --train==1  --test==1
-python xshap.py  --remg  --train==1  --test==2
-python visualization2.py  --remg  --train==1  --test==2
-python filter_figs.py  --remg  --train==1  --test==2
-python xshap.py  --remg  --train==1  --test==3
-python visualization2.py  --remg  --train==1  --test==3
-python filter_figs.py  --remg  --train==1  --test==3
+python xshap.py  --remg  --train=1  --test=1
+python visualization2.py  --remg  --train=1  --test=1
+python filter_figs.py  --remg  --train=1  --test=1
+python xshap.py  --remg  --train=1  --test=2
+python visualization2.py  --remg  --train=1  --test=2
+python filter_figs.py  --remg  --train=1  --test=2
+python xshap.py  --remg  --train=1  --test=3
+python visualization2.py  --remg  --train=1  --test=3
+python filter_figs.py  --remg  --train=1  --test=3
 #################
 python train.py  --remg --rmot
-python xshap.py  --remg --rmot --train==1  --test==1
-python visualization2.py  --remg --rmot --train==1  --test==1
-python filter_figs.py  --remg --rmot --train==1  --test==1
-python xshap.py  --remg --rmot --train==1  --test==2
-python visualization2.py  --remg --rmot --train==1  --test==2
-python filter_figs.py  --remg --rmot --train==1  --test==2
-python xshap.py  --remg --rmot --train==1  --test==3
-python visualization2.py  --remg --rmot --train==1  --test==3
-python filter_figs.py  --remg --rmot --train==1  --test==3
+python xshap.py  --remg --rmot --train=1  --test=1
+python visualization2.py  --remg --rmot --train=1  --test=1
+python filter_figs.py  --remg --rmot --train=1  --test=1
+python xshap.py  --remg --rmot --train=1  --test=2
+python visualization2.py  --remg --rmot --train=1  --test=2
+python filter_figs.py  --remg --rmot --train=1  --test=2
+python xshap.py  --remg --rmot --train=1  --test=3
+python visualization2.py  --remg --rmot --train=1  --test=3
+python filter_figs.py  --remg --rmot --train=1  --test=3
 #################
 python train.py --regg  
-python xshap.py --regg   --train==1  --test==1
-python visualization2.py --regg   --train==1  --test==1
-python filter_figs.py --regg   --train==1  --test==1
-python xshap.py --regg   --train==1  --test==2
-python visualization2.py --regg   --train==1  --test==2
-python filter_figs.py --regg   --train==1  --test==2
-python xshap.py --regg   --train==1  --test==3
-python visualization2.py --regg   --train==1  --test==3
-python filter_figs.py --regg   --train==1  --test==3
+python xshap.py --regg   --train=1  --test=1
+python visualization2.py --regg   --train=1  --test=1
+python filter_figs.py --regg   --train=1  --test=1
+python xshap.py --regg   --train=1  --test=2
+python visualization2.py --regg   --train=1  --test=2
+python filter_figs.py --regg   --train=1  --test=2
+python xshap.py --regg   --train=1  --test=3
+python visualization2.py --regg   --train=1  --test=3
+python filter_figs.py --regg   --train=1  --test=3
 #################
 python train.py --regg  --rmot
-python xshap.py --regg  --rmot --train==1  --test==1
-python visualization2.py --regg  --rmot --train==1  --test==1
-python filter_figs.py --regg  --rmot --train==1  --test==1
-python xshap.py --regg  --rmot --train==1  --test==2
-python visualization2.py --regg  --rmot --train==1  --test==2
-python filter_figs.py --regg  --rmot --train==1  --test==2
-python xshap.py --regg  --rmot --train==1  --test==3
-python visualization2.py --regg  --rmot --train==1  --test==3
-python filter_figs.py --regg  --rmot --train==1  --test==3
+python xshap.py --regg  --rmot --train=1  --test=1
+python visualization2.py --regg  --rmot --train=1  --test=1
+python filter_figs.py --regg  --rmot --train=1  --test=1
+python xshap.py --regg  --rmot --train=1  --test=2
+python visualization2.py --regg  --rmot --train=1  --test=2
+python filter_figs.py --regg  --rmot --train=1  --test=2
+python xshap.py --regg  --rmot --train=1  --test=3
+python visualization2.py --regg  --rmot --train=1  --test=3
+python filter_figs.py --regg  --rmot --train=1  --test=3
 #################
 python train.py --regg --remg 
-python xshap.py --regg --remg  --train==1  --test==1
-python visualization2.py --regg --remg  --train==1  --test==1
-python filter_figs.py --regg --remg  --train==1  --test==1
-python xshap.py --regg --remg  --train==1  --test==2
-python visualization2.py --regg --remg  --train==1  --test==2
-python filter_figs.py --regg --remg  --train==1  --test==2
-python xshap.py --regg --remg  --train==1  --test==3
-python visualization2.py --regg --remg  --train==1  --test==3
-python filter_figs.py --regg --remg  --train==1  --test==3
+python xshap.py --regg --remg  --train=1  --test=1
+python visualization2.py --regg --remg  --train=1  --test=1
+python filter_figs.py --regg --remg  --train=1  --test=1
+python xshap.py --regg --remg  --train=1  --test=2
+python visualization2.py --regg --remg  --train=1  --test=2
+python filter_figs.py --regg --remg  --train=1  --test=2
+python xshap.py --regg --remg  --train=1  --test=3
+python visualization2.py --regg --remg  --train=1  --test=3
+python filter_figs.py --regg --remg  --train=1  --test=3
 #################
