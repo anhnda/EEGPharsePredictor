631df07b62071d2624ba9037fdf557533b3efe25
Duc Anh Nguyen <anhnd@Ducs-MacBook-Pro-2.local>
2025-09-26 22:39:44 +0700

    Upload tdev

---
+++ setup_env.sh
@@ -0,0 +1,19 @@
+# create and activate conda env
+#conda create -n EEGPP python=3.10
+#conda activate EGGPP
+
+# install libs
+pip3 install torch torchvision
+conda install -c conda-forge lightning==2.4.0 -y
+conda install -c conda-forge torchinfo==1.8.0 -y
+conda install -c conda-forge pyyaml==6.0.2 -y
+conda install -c conda-forge scikit-learn==1.5.2 -y
+conda install -c conda-forge seaborn=0.13.2 -y
+conda install -c conda-forge pandas==2.2.2 -y
+conda install -c conda-forge scipy==1.14.1 -y
+conda install -c conda-forge numpy==2.1.1 -y
+conda install -c conda-forge pywavelets==1.7.0 -y
+pip install dropbox==12.0.2
+pip install joblib==1.4.2
+pip install ptwt==0.1.9
+pip install python-dotenv==1.0.1
--- src/eegpp2/models/baseline/fft.py
+++ src/eegpp2/models/baseline/fft.py
@@ -1,13 +1,15 @@
 import torch
-from torch import nn
-from torch.nn import MaxPool1d
-import params
+
+from ... import params
+
+
 class FFTModel(torch.nn.Module):
     def __init__(self, n_class):
         super().__init__()
         self.name = "FFT"
         self.dropout1 = torch.nn.Dropout(0.1)
-        self.ff1 = torch.nn.Linear(params.MAX_SEQ_SIZE, params.MAX_SEQ_SIZE)
+
+        self.ff1 = torch.nn.Linear(params.MAX_SEQ_SIZE * 3, params.MAX_SEQ_SIZE)
         self.ff2 = torch.nn.Linear(params.MAX_SEQ_SIZE, n_class)
         self.act = torch.nn.ReLU()
 
@@ -18,5 +20,4 @@ class FFTModel(torch.nn.Module):
         x = self.act(self.ff1(x))
         x = self.ff2(x)
         x = torch.squeeze(x, 1)
-
         return x
